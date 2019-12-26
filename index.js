4b28ad9ae77ebc4ed9461ea34c1617f6243d91b7
kushaalrana <ranakushaal@gmail.com>
2019-12-26 09:45:58 +0530

    resumeupdatedagain

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT ||3000
-
-app.get('/', (req, res) => res.sendFile('__dirname+ '/resume.html'))
-
+app.use(express.static('Resume'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
