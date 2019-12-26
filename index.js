fcd9bb0cbb9601f6ff0513800e314b348ad9831e
Kushaal <43180237+kushaalrana@users.noreply.github.com>
2019-12-26 10:49:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT ||3000
-app.use(express.static('Resume'))
+app.use(express.static('res'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/resume.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
